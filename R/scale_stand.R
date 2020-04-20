84878f6e663566009739cf7c8660ac4ba813539c
Trang Le <grixor@gmail.com>
2020-04-19 21:12:42 -0400

    update function descriptions

--- R/scale_stand.R
+++ R/scale_stand.R
@@ -1,13 +1,17 @@
+#' Performs transformation of continuous variables for the heatmap color scales.
+#'
+#' @param x Numeric vector.
+#' @param trans_type Character string of 'scale' or 'normalize'.
+#' If 'scale', subtract the mean and divide by the standard deviation.
+#' If 'normalize', i.e., max-min normalize, subtract the min and divide by the max.
+#' More information on what transformation to choose can be acquired here:
+#' https://cran.rstudio.com/web/packages/heatmaply/vignettes/heatmaply.html#scale
+#'
+#' @return Numeric vector of transformed x.
+#' @export
+#'
+#' @examples
 scale_stand <- function(x, trans_type){
-  # This function performs transformation of continuous variables.
-  # 
-  # either scale (i.e., subtract the mean and divide by the standard deviation) 
-  # or max-min normalize (i.e., subtract the min and divide by the max)
-  # only for the heatmap color scales.
-  # 
-  # More information on what transformation to choose can be acquired here:
-  # https://cran.rstudio.com/web/packages/heatmaply/vignettes/heatmaply.html#scale
-  
   if (trans_type == 'scale') {
     scale(x)
   } else if (trans_type == 'normalize'){
