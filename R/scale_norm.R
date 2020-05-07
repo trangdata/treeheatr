048e5a293ddb799a24e5d885a272e556f2885449
Trang Le <grixor@gmail.com>
2020-05-07 17:12:09 -0400

    utilize switch

--- R/scale_norm.R
+++ R/scale_norm.R
@@ -1,23 +1,29 @@
 #' Performs transformation of continuous variables for the heatmap color scales.
 #'
 #' @param x Numeric vector.
-#' @param trans_type Character string of 'scale' or 'normalize'.
+#' @param trans_type Character string of 'normalize', 'scale' or 'none'.
 #' If 'scale', subtract the mean and divide by the standard deviation.
 #' If 'normalize', i.e., max-min normalize, subtract the min and divide by the max.
+#' If 'none', no transformation is applied.
 #' More information on what transformation to choose can be acquired here:
 #' https://cran.rstudio.com/web/packages/heatmaply/vignettes/heatmaply.html#scale
 #'
-#' @return Numeric vector of transformed x.
+#' @return Numeric vector of the transformed `x`.
 #' @export
 #'
-scale_norm <- function(x, trans_type){
-  if (trans_type == 'scale') {
-    scale(x)
-  } else if (trans_type == 'normalize'){
-    x <- x - min(x, na.rm = T)
-    x <- x/max(x, na.rm = T)
-    x
-  } else {
-    x
-  }
+scale_norm <- function(x, trans_type = c('normalize', 'scale', 'none')){
+  trans_type <- match.arg(trans_type)
+
+  switch(
+    trans_type,
+    scale = scale(x),
+    normalize = my_norm(x),
+    none = x
+  )
+}
+
+my_norm <- function(x){
+  x <- x - min(x, na.rm = T)
+  x <- x/max(x, na.rm = T)
+  x
 }
