dce5aed4f0d6e8ff224b75c2b5353c1b189e5ceb
Trang Le <grixor@gmail.com>
2020-05-08 13:55:35 -0400

    separate clust() into 2 smaller funcs

--- R/clust.R
+++ R/clust.R
@@ -1,4 +1,4 @@
-#' Performs clustering of samples or features.
+#' Performs clustering of samples.
 #'
 #' @import dplyr
 #' @importFrom stats hclust
@@ -9,32 +9,48 @@
 #' @param clust_samps if TRUE, clusters samples within each leaf_node using Gower metric
 #' based on the values of the variables in clust_vec
 #' and returns the ordered dataframe for each leaf_node.
+#'
+#' @return Dataframe of reordered original dataset when clust_samps == TRUE.
+#' @export
+#'
+#'
+clust_samp_func <- function(leaf_node = NULL, dat, clust_vec, clust_samps){
+  df <- dat[dat$node_id == leaf_node, ]
+
+  if (clust_samps && length(clust_vec) > 1){
+    new_samp_order <- df[, clust_vec] %>%
+      cluster::daisy(metric = 'gower') %>%
+      hclust() %>%
+      `[[`('order')
+
+    df[new_samp_order, ] # return ordered dataframe
+  } else {
+    df
+  }
+}
+
+
+# ----------------------------------------------------------------------
+#'
+#' Performs clustering or features.
+#'
+#' @import dplyr
+#' @importFrom stats hclust
+#' @param dat Dataframe of the original dataset. Samples may be reordered.
+#' @param clust_vec Character vector of variable names to be applied clustering on.
+#' Can include class labels.
 #' @param clust_feats if TRUE clusters displayed features (passed through clust_vec)
 #' using the the Gower metric based on the values of all samples
 #' and returns the ordered features.
 #' When clust_samps = FALSE and clust_feats = FALSE, no clustering is performed.
 #'
-#' @return Dataframe of reordered original dataset when clust_samps == TRUE.
-#' Character vector of reordered features when clust_feats == TRUE.
+#' @return Character vector of reordered features when clust_feats == TRUE.
 #' @export
 #'
-#' @examples
-clust <- function(leaf_node = NULL, dat, clust_vec, clust_samps = TRUE, clust_feats = FALSE){
-  if (clust_samps == TRUE){
-    df <- dat[dat$node_id == leaf_node, ]
+#'
+clust_feat_func <- function(dat, clust_vec, clust_feats){
 
-    if (length(clust_vec) > 1){
-      new_samp_order <- df[, clust_vec] %>%
-        cluster::daisy(metric = 'gower') %>%
-        hclust() %>%
-        `[[`('order')
-
-      df[new_samp_order, ] # return ordered dataframe
-    } else {
-      df
-    }
-
-  } else if (clust_feats == TRUE) {
+  if (clust_feats){
     new_feat_order <- dat[, clust_vec] %>%
       as.matrix() %>%
       t() %>%
@@ -43,9 +59,7 @@ clust <- function(leaf_node = NULL, dat, clust_vec, clust_samps = TRUE, clust_fe
       `[[`('order')
 
     clust_vec[new_feat_order] # return ordered features, not dataframe
-
-  } else { # no clustering
-    dat
+  } else {
+    clust_vec
   }
 }
-
