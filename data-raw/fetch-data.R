903177fedcc475b86333d858217b4c494aa881b7
Trang Le <grixor@gmail.com>
2020-05-12 18:20:44 -0400

    update data documentation

--- data-raw/fetch-data.R
+++ data-raw/fetch-data.R
@@ -5,13 +5,12 @@ waveform <- pmlblite::fetch_data('waveform-40')
 mushroom <- pmlblite::fetch_data('mushroom')
 titanic <- titanic::titanic_train
 flags <- pmlblite::fetch_data('flags')
-diabetes <- pmlblite::fetch_data('diabetes')
 saheart <- pmlblite::fetch_data('saheart')
 pollen <- pmlblite::fetch_data('529_pollen')
 galaxy <- pmlblite::fetch_data('690_visualizing_galaxy')
 
 data(wine, package = 'rattle')
 
-usethis::use_data(wine_quality_red, waveform, mushroom, titanic, flags,
-                  diabetes, saheart, wine, pollen, galaxy,
+usethis::use_data(wine_quality_red, waveform, mushroom,
+                  titanic, flags, saheart, wine, pollen, galaxy,
                   overwrite = TRUE)
